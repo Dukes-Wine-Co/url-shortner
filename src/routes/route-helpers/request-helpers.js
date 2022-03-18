10fcbe919468c3afc327fceb98a5aa9b259f5c04
Tyrone Scafe <tscafejr@gmail.com>
2022-03-18 14:36:20 -0700

    fix comma in string

--- src/routes/route-helpers/request-helpers.js
+++ src/routes/route-helpers/request-helpers.js
@@ -7,7 +7,7 @@ const REDIRECT_MAP = {
     'virtual-tasting': process.env.ZOOM_LINK,
     'juneteenth-2020': 'https://www.dukeswines.com/our-story',
     'trends-1': 'https://issuu.com/trendspublishing/docs/trends_mayjune21_emag?fr=sYTFjMjMwNjIzMg',
-    'catalog-fall-2021': 'https://static1.squarespace.com/static/5dde0017fbd78f1f18e94e3c/t/61902046f8750318f4aa8adc/1636835398996/2021_Fall_Catalog.pdf,',
+    'catalog-fall-2021': 'https://static1.squarespace.com/static/5dde0017fbd78f1f18e94e3c/t/61902046f8750318f4aa8adc/1636835398996/2021_Fall_Catalog.pdf',
     'catalog-winter-2021': 'https://static1.squarespace.com/static/5dde0017fbd78f1f18e94e3c/t/61debbb036f857659c36ea03/1641986993344/2021_winter_wine_catalog.pdf',
     'mahalome': 'https://mahalome.com/'
 };
